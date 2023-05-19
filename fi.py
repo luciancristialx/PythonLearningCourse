304bfe607641db979bd065b63d118047a392e46b
luciancristialx <alexandruluciancristi@gmail.com>
2023-05-19 15:09:04 +0300

    Update with Video training 22,23

---
+++ fi.py
@@ -0,0 +1,11 @@
+# name = "dev"
+# print(name)
+# name = 23
+# print(name)
+
+a: int  # declare variable of type int (a-z A-Z 0-9 _)
+
+a = 5
+a = 13
+a = "alex"
+print(a)
--- variables.py
+++ variables.py
@@ -1,10 +1,16 @@
-x = 5
-y = 6
-z = 5 + y
-c = x+y
+# x = 5
+# y = 6
+# z = 5 + y
+# c = x+y
 
-#print(z)
-#print(c)
+# print(z)
+# print(c)
 
-a=3+5*6
-print(a)
+# a=(3+5)*6
+# print(a)
+
+x = 3 + 1.2 - 1
+y = ((3 + 5) * 2) ** 2 / 16 - 1
+
+print(x)
+print(y)
