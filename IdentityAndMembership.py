ca45db23b5b4c5d313558272da4fe929bfb9e484
luciancristialx <alexandruluciancristi@gmail.com>
2023-06-20 11:39:49 +0300

    Video training 34,35,36,37,38,39,40

---
+++ IdentityAndMembership.py
@@ -0,0 +1,27 @@
+# Types:
+#   Identity operators
+#       1.is
+#       2.is not
+
+x: int
+y: int
+z: int
+
+x = 3
+y = 3
+z = 5
+
+print(x is y)
+print(x is not z)
+
+#   Membership operators
+#       1.in
+#       2.not in
+
+a = [1, 2, 3, 4]
+print(1 in a)
+
+b = "water is important"
+print("a" not in b)
+
+#   Bitwise operators
