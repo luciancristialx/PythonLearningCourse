7891381ef47ef38f4d42f0542bb70b06e0ea6434
luciancristialx <alexandruluciancristi@gmail.com>
2023-06-26 17:51:35 +0300

    Video training 56, 57, 58, 59 .... 66

---
+++ TypesOfErrors.py
@@ -0,0 +1,9 @@
+# Types of errors:
+#   1. Syntax Error
+#   2. Indentation Error
+#   3. Type Error
+#   4. Name Error
+#   5. Zero Division Error
+#   6. Index Error
+#   7. Runtime Error
+#   8. Attribute Error
